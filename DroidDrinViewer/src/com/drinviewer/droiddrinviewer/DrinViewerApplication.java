2db4565c7449dfaa2803a94bd2ce6afc45caf8f1
Giorgio Consorti <g.consorti@lynxlab.com>
2014-01-23 23:09:57 +0100

    added ACRA library to send crash reports by email

--- DroidDrinViewer/src/com/drinviewer/droiddrinviewer/DrinViewerApplication.java
+++ DroidDrinViewer/src/com/drinviewer/droiddrinviewer/DrinViewerApplication.java
@@ -1,7 +1,17 @@
 package com.drinviewer.droiddrinviewer;
 
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
 import android.app.Application;
 import android.content.Intent;
+
+@ReportsCrashes(formKey="",
+mode = ReportingInteractionMode.TOAST,
+mailTo = "drinviewer@gmail.com",
+resToastText = R.string.crash_toast_text // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+)
 
 public class DrinViewerApplication extends Application {
 	private static String installationUUID;
@@ -12,6 +22,8 @@ public class DrinViewerApplication extends Application {
 		// sets installation uuid
 		DrinViewerApplication.installationUUID = InstallationUUID.id(getApplicationContext());
 		startService(new Intent(DiscoverServerService.class.getName()));
+			
+		ACRA.init(this);
 	}
 
 	@Override

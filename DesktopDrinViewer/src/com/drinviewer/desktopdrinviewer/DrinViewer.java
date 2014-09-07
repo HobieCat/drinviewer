18155714f865fc1821f3c7232e7d76283312d7d1
Giorgio Consorti <giorgio.consorti@gmail.com>
2014-09-07 21:44:08 +0200

    handle SHOW_PAIRED action, new settings menu with font selection and improved saving of Preferences

--- DesktopDrinViewer/src/com/drinviewer/desktopdrinviewer/DrinViewer.java
+++ DesktopDrinViewer/src/com/drinviewer/desktopdrinviewer/DrinViewer.java
@@ -22,12 +22,18 @@ package com.drinviewer.desktopdrinviewer;
 
 import java.net.BindException;
 import java.text.MessageFormat;
+import java.util.StringTokenizer;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FontDialog;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
@@ -57,7 +63,17 @@ public class DrinViewer {
 	 * - position of the popup defined by user
 	 * 
 	 */
-	private Preferences prefs;
+	private final Preferences prefs = DrinViewer.getPreferences();
+	
+	/**
+	 * SWT display object
+	 */
+	private Display display;
+	
+	/**
+	 * SWT shell object
+	 */
+	private Shell shell;
 
 	public void runApplication(String[] args) {
 		// if an argument is passed, it's the comm port
@@ -68,14 +84,13 @@ public class DrinViewer {
 			}
 		}
 		
-		prefs = Preferences.userRoot().node(this.getClass().getName());
 		boolean serverWasRunning = prefs.getBoolean(DesktopDrinViewerConstants.PREFS_SERVER, true);
 		
 		Display.setAppName(Constants.APPNAME);
 		Display.setAppVersion(Constants.APPVERSION);		
 		
-		final Display display = new Display();
-		final Shell shell = new Shell(display);
+		this.display = new Display();
+		this.shell = new Shell(display);
 		final Menu menu = new Menu(shell, SWT.POP_UP);
 
 		// icon image for the system tray
@@ -150,6 +165,8 @@ public class DrinViewer {
 				    public void run() {
 				    	if (event.action == Constants.SHOW_POPUP) {
 				    		ndlg.notify(event.title, event.message, event.imageData, false);
+				    	} else if (event.action == Constants.SHOW_PAIRED) {
+				    		ndlg.notify(event.title, event.message, event.imageData, true);
 				    	} else if (event.action == Constants.REMOVE_POPUP) {
 				    		ndlg.removeNotify(event.title, event.message);
 				    	}
@@ -205,6 +222,11 @@ public class DrinViewer {
 					try {						
 						ds.startServer();
 						prefs.putBoolean(DesktopDrinViewerConstants.PREFS_SERVER, true);
+						try {
+							prefs.flush();
+						} catch (BackingStoreException e) {
+							e.printStackTrace();
+						}
 						item.setImage(normalImage);
 						item.setToolTipText(baseToolTipText+" - "+DesktopDrinViewerConstants.i18nMessages.getString("enabled"));
 					} catch (BindException e) {
@@ -225,6 +247,11 @@ public class DrinViewer {
 				public void handleEvent(Event event) {
 					ds.stopServer();
 					prefs.putBoolean(DesktopDrinViewerConstants.PREFS_SERVER, false);
+					try {
+						prefs.flush();
+					} catch (BackingStoreException e) {
+						e.printStackTrace();
+					}
 					item.setImage(disabledImage);
 					item.setToolTipText(baseToolTipText+" - "+DesktopDrinViewerConstants.i18nMessages.getString("disabled"));
 					stopServer.setEnabled(false);
@@ -233,19 +260,68 @@ public class DrinViewer {
 			});
 			
 			/**
-			 * set popup position menu item
+			 * settings menu item
 			 */
-			MenuItem popupPosition = new MenuItem(menu, SWT.PUSH);
+			MenuItem settingsMenu = new MenuItem(menu, SWT.CASCADE);
+			settingsMenu.setText(DesktopDrinViewerConstants.i18nMessages.getString("settings"));
+			
+			/**
+			 * settings submenu
+			 */
+			Menu settingsSubMenu = new Menu (menu);
+			settingsMenu.setMenu (settingsSubMenu);
+			
+			/**
+			 * settings/popupposition menu item 
+			 */
+			MenuItem popupPosition = new MenuItem (settingsSubMenu, SWT.PUSH);
 			popupPosition.setText(DesktopDrinViewerConstants.i18nMessages.getString("setpopupposition"));
 			popupPosition.addListener(SWT.Selection, new Listener() {
 				@Override
 				public void handleEvent(Event event) {
 					display.asyncExec(new Runnable() {
+						@Override						
 					    public void run() {
 					    	ndlg.showToSetPosition(DesktopDrinViewerConstants.i18nMessages.getString("popuptitle"),
-					    			DesktopDrinViewerConstants.i18nMessages.getString("popuptext"), null);					    	
+					    			DesktopDrinViewerConstants.i18nMessages.getString("popuptext"), null);
 					    }
 					});
+				}
+			});
+			
+			/**
+			 * settings/select font menu item
+			 */
+			MenuItem fontMenu = new MenuItem(settingsSubMenu, SWT.CASCADE);
+			fontMenu.setText(DesktopDrinViewerConstants.i18nMessages.getString("selectfont"));
+			
+			/**
+			 * settings/select font submenu
+			 */
+			Menu fontSubMenu = new Menu (settingsSubMenu);
+			fontMenu.setMenu (fontSubMenu);
+			
+			/**
+			 * settings/select font/select title font menu item 
+			 */
+			MenuItem selectTitleFont = new MenuItem (fontSubMenu, SWT.PUSH);
+			selectTitleFont.setText(DesktopDrinViewerConstants.i18nMessages.getString("selecttitlefont"));
+			selectTitleFont.addListener(SWT.Selection, new Listener(){
+				@Override
+				public void handleEvent(Event event) {
+					selectAndSaveFont(true);
+				}
+			});
+			
+			/**
+			 * settings/select font/select text title font menu item 
+			 */
+			MenuItem selectTextFont = new MenuItem(fontSubMenu, SWT.PUSH);
+			selectTextFont.setText(DesktopDrinViewerConstants.i18nMessages.getString("selecttextfont"));
+			selectTextFont.addListener(SWT.Selection, new Listener(){
+				@Override
+				public void handleEvent(Event event) {
+					selectAndSaveFont(false);
 				}
 			});
 			
@@ -259,7 +335,7 @@ public class DrinViewer {
 				public void handleEvent(Event event) {
 					display.asyncExec(new Runnable() {
 					    public void run() {
-					    	 ndlg.notify("Incoming Call", "Test Number:\n+39-666-666-66-66",null, true);
+					    	 ndlg.notify(DesktopDrinViewerConstants.i18nMessages.getString("testnotify"), "+39-666-666-66-66",null, true);
 					    }
 					});
 				}
@@ -307,9 +383,85 @@ public class DrinViewer {
 	}
 	
 	/**
+	 * shows font dialog and save user choice to preferences
+	 * 
+	 * @param isTitle true if must set font for title, false for text
+	 */
+	private void selectAndSaveFont (final boolean isTitle) {
+		display.asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				FontDialog fd = new FontDialog(shell);
+		        fd.setText(DesktopDrinViewerConstants.i18nMessages.getString("selectfont"));
+		        fd.setRGB(new RGB(0, 0, 0));
+		        fd.setFontList(DrinViewer.getNotificationFont(display, isTitle));
+		        if (fd.open() != null) {
+		        	// save font
+		    		String namePref  = (isTitle) ? DesktopDrinViewerConstants.PREFS_TITLEFONTNAME : 
+						   DesktopDrinViewerConstants.PREFS_TEXTFONTNAME;
+		    		String sizePref  = (isTitle) ? DesktopDrinViewerConstants.PREFS_TITLEFONTSIZE :
+						   DesktopDrinViewerConstants.PREFS_TEXTFONTSIZE;
+		    		String stylePref = (isTitle) ? DesktopDrinViewerConstants.PREFS_TITLEFONTSTYLE :
+						   DesktopDrinViewerConstants.PREFS_TEXTFONTSTYLE;
+		    		
+		    		prefs.put(namePref, fd.getFontList()[0].getName());
+		    		prefs.putInt(sizePref, fd.getFontList()[0].getHeight());
+		    		prefs.putInt(stylePref, fd.getFontList()[0].getStyle());
+		    		try {
+						prefs.flush();
+					} catch (BackingStoreException e) {
+						e.printStackTrace();
+					}
+		        }
+			}
+		});		
+	}
+
+	/**
+	 * get font for notification
+	 * 
+	 * @param display display in use
+	 * @param isTitle true if must get font for title, false for text
+	 * 
+	 * @return FontData
+	 */
+	public static FontData[] getNotificationFont(Display display, boolean isTitle) {
+		Preferences prefs = DrinViewer.getPreferences();
+		FontData fd = display.getSystemFont().getFontData()[0];
+		
+		String namePref  = (isTitle) ? DesktopDrinViewerConstants.PREFS_TITLEFONTNAME : 
+									   DesktopDrinViewerConstants.PREFS_TEXTFONTNAME;
+		String sizePref  = (isTitle) ? DesktopDrinViewerConstants.PREFS_TITLEFONTSIZE :
+									   DesktopDrinViewerConstants.PREFS_TEXTFONTSIZE;
+		String stylePref = (isTitle) ? DesktopDrinViewerConstants.PREFS_TITLEFONTSTYLE :
+									   DesktopDrinViewerConstants.PREFS_TEXTFONTSTYLE;
+		
+		String fontName = prefs.get(namePref, fd.getName());
+		int fontSize = prefs.getInt(sizePref, fd.getHeight());
+		int fontStyle = prefs.getInt(stylePref, (isTitle) ? SWT.BOLD : fd.getStyle());
+
+		return (new Font(display, fontName, fontSize, fontStyle)).getFontData(); 
+	}
+	
+	/**
+	 * get application Preferences object
+	 * 
+	 * @return Preferences
+	 */
+	public static Preferences getPreferences() {		
+		// be a nice guy and save preferences to a nice location:
+		// com/drinviewer/desktopdrinviewer/DrinViewer
+		Preferences prefs = Preferences.userRoot();		
+		StringTokenizer strTkn = new StringTokenizer(DrinViewer.class.getName(), ".");
+		while(strTkn.hasMoreTokens()) {
+			prefs = prefs.node(strTkn.nextToken());
+		}		
+		return prefs;
+	}
+	
+	/**
 	 * handle server is already running or port in
 	 * use error by displaying a dialog to the user
-	 * 
 	 */
 	private static void alreadyRunningError(Shell shell) {
 		
